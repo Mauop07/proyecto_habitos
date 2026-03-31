16718cd7856bfe6dd63654472cec23f238a0d96e
Mauricio Orellana <25000174@galileo.edu>
2026-03-31 00:19:56 -0600

    Refactor CORS handling in index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,16 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('/*', cors());
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Origin', 'https://proyecto-habitos-front.vercel.app');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(express.json());
 
